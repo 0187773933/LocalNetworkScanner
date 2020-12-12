8d59001c8f0d70e80cca33abc218c3e68bdb7c11
0187773933 <collincerbus@student.olympic.edu>
2020-12-12 00:18:20 -0500

    4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/0187773933/LocalNetworkScanner
 
 go 1.15
+
+require github.com/jackpal/gateway v1.0.6
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	localnetwork "localnetwork"
-)
-
-func main() {
-	fmt.Println( GetIPAddressFromMacAddress( strings.Replace( strings.ToLower( "2C-64-1F-25-6B-3C" ) , "-" , ":" , -1 ) ) )
-}
